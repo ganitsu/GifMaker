f64ea58155b32375651f12c174531cede9590de4
nitsuga2 <giulianiagustin7@gmail.com>
2025-05-10 21:02:16 -0300

    Nuevo?

--- src/components/ProcessedGifPreview.jsx
+++ src/components/ProcessedGifPreview.jsx
@@ -1,19 +1,7 @@
 import React, { useRef, useState, useEffect } from 'react';
 import { applyMask } from './helps';
 
-// Helper que aplica una máscara a una imagen original
 
-
-// Helper para cargar imagen como objeto Image
-function loadImage(url) {
-  return new Promise((resolve, reject) => {
-    const img = new Image();
-    img.crossOrigin = 'Anonymous';
-    img.src = url;
-    img.onload = () => resolve(img);
-    img.onerror = reject;
-  });
-}
 
 export default function ProcessedGifPreview({ images, original_images, fps }) {
   const canvasRef = useRef(null);
